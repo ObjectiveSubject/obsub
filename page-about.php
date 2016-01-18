a7bb2c15330c8fd330eb34872fe367c022a36e1b
Kirk Pettinga <kirk.pettinga@gmail.com>
2016-01-18 12:04:32 +0700

    add about template

---
+++ page-about.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * @package obsub
+ */
+
+get_header(); ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<section class="page-header outer-container">
+			<div class="inner-container">
+				<?php the_content(); ?>
+			</div>
+		</section>
+
+	<?php endwhile; // end of the loop. ?>
+		
+<?php get_footer(); ?>
