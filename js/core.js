4acba5a507e02a03e1500b73efd0c1e7c6fb102d
Kirk Pettinga <kirk.pettinga@gmail.com>
2016-01-19 14:52:39 +0700

    office map interaction

--- js/core.js
+++ js/core.js
@@ -153,25 +153,72 @@
 
 (function( $, window, undefined ){
 
-	if ( $('#content.contact').length > 0 ) {
+	var $window = $(window),
+		winHeight = $window.height(),
+		mediaSize = getMediaSize(),
+		$brooklyn, brooklynTop, 
+		$oakland, oaklandTop,
+		$addressBar;
 
-		mapboxgl.accessToken = 'pk.eyJ1Ijoib2JqZWN0aXZlc3ViamVjdCIsImEiOiJPY25wYWRjIn0.AFZPHessR_DGefRkzPilDA';
+	if ( $('.office-map').length > 0 && Modernizr.cssvhunit ) {
+
+		$brooklyn 	 = $('.office-location.brooklyn'),
+		$oakland 	 = $('.office-location.oakland'),
+		$addressBar  = $('.address-sidebar');
+
+		brooklynTop  = $brooklyn.offset().top,
+		oaklandTop 	 = $oakland.offset().top,
 		
-		var brooklyn = new mapboxgl.Map({
-		    container: 'brooklyn-map', // container id
-		    style: 'mapbox://styles/objectivesubject/cijdnlkv400hpavm3hkpvu33m', //stylesheet location
-		    // center: [-74.50, 40], // starting position
-		    // zoom: 9 // starting zoom
-		    scrollWheelZoom: false
-		});
-		var oakland = new mapboxgl.Map({
-		    container: 'oakland-map', // container id
-		    style: 'mapbox://styles/objectivesubject/cijdoxa6000f81mm2bsxybv8w', //stylesheet location
-		    // center: [-74.50, 40], // starting position
-		    // zoom: 9 // starting zoom
-		    scrollWheelZoom: false
-		});
-	
+		$window.on("resize", onResize);
+		$window.on("scroll", onScroll);
+	}
+
+	function onResize() {
+		winHeight = $window.height();
+		brooklynTop = $brooklyn.offset().top;
+		oaklandTop = $oakland.offset().top;
+		mediaSize = getMediaSize();
+	}
+
+	function onScroll() {
+
+		var scrollTop = $window.scrollTop(),
+			scrollMid = scrollTop + (winHeight / 2);
+
+		if ( mediaSize == "medium" ) {
+
+			if ( scrollTop >= brooklynTop && scrollTop < oaklandTop ) {
+				$brooklyn
+					.addClass('fixed')
+					.removeClass('offset');
+				$addressBar
+					.addClass('fixed')
+					.removeClass('offset');
+			} else if ( scrollTop >= oaklandTop ) {
+				$brooklyn
+					.removeClass('fixed')
+					.addClass('offset');
+				$addressBar
+					.removeClass('fixed')
+					.addClass('offset');
+			} else {
+				$brooklyn.removeClass('fixed offset');
+				$addressBar.removeClass('fixed offset');
+			}
+
+			if ( scrollMid >= oaklandTop ) {
+				$addressBar.attr("data-show", "oakland");
+			} else {
+				$addressBar.attr("data-show", "brooklyn");
+			}
+
+		}
+
+	}
+
+	function getMediaSize() {
+		//detect if desktop/mobile
+		return window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
 	}
 
 })(jQuery, window);
--- page-contact.php
+++ page-contact.php
@@ -11,8 +11,8 @@ get_header(); ?>
 
 		<section class="page-header">
 			<div class="inner-container">
-				<?php the_content(); ?>
-				<h2 class="future-A">
+				<h1 class="page-title">We’re based in Brooklyn, NY and Oakland, CA.</h1>
+				<h2 class="future-A page-subtitle">
 					718.801.8075<br/>
 					<a href="mailto:info@objectivesubject.com" class="text-color-link">info@objectivesubject.com</a>
 				</h2>
@@ -20,39 +20,73 @@ get_header(); ?>
 		</section>
 
 		<section class="page-section office-locations clearfix">
-			<div class="office-location brooklyn">
-				<div id="brooklyn-map" class="office-map"></div>
-				<address class="h2 future-A">
-					81 prospect street<br/>
-					brooklyn, ny 11201
-				</address>
-				<p class="push">
-					<em>Directions</em><br/>
-					F to York St,<br/>
-					A and C to High St,<br/>
-					2 and 3 to Clark St
-				</p>
+			<div class="office-location brooklyn clearfix">
+				<div id="brooklyn-map" class="office-map brooklyn-map"></div>
+				<div class="office-address">
+					<address class="h2 hug future-A">
+						81 prospect street<br/>
+						brooklyn, ny 11201
+					</address>
+					<p class="directions push">
+						<em>Directions</em><br/>
+						F to York St,<br/>
+						A and C to High St,<br/>
+						2 and 3 to Clark St
+					</p>
+				</div>
 			</div>
-			<div class="office-location oakland">
-				<div id="oakland-map" class="office-map"></div>
-				<address class="h2 future-A">
-					2355 broadway, no 402<br/>
-					oakland, ca 94612
-				</address>
-				<p class="push">
-					<em>Directions</em><br/>
-					BART: 19th St - Oakland<br/>
-					From San Francisco:<br/>
-					I-580 exit 20 towards Broadway
-				</p>
+			<div class="office-location oakland clearfix">
+				<div id="oakland-map" class="office-map oakland-map"></div>
+				<div class="office-address">
+					<address class="h2 hug future-A">
+						2355 broadway<br/>
+						no. 402<br/>
+						oakland, ca 94612
+					</address>
+					<p class="directions push">
+						<em>Directions</em><br/>
+						BART: 19th St - Oakland<br/>
+						From San Francisco:<br/>
+						I-580 exit 20 towards Broadway
+					</p>
+				</div>
+			</div>
+			<div class="address-sidebar" data-show="brooklyn">
+				<div class="office-address brooklyn">
+					<address class="h2 hug future-A">
+						81 prospect street<br/>
+						brooklyn, ny 11201
+					</address>
+					<p class="directions push">
+						<em>Directions</em><br/>
+						F to York St,<br/>
+						A and C to High St,<br/>
+						2 and 3 to Clark St
+					</p>
+				</div>
+				<div class="office-address oakland">
+					<address class="h2 hug future-A">
+						2355 broadway<br/>
+						no. 402<br/>
+						oakland, ca 94612
+					</address>
+					<p class="directions push">
+						<em>Directions</em><br/>
+						BART: 19th St - Oakland<br/>
+						From San Francisco:<br/>
+						I-580 exit 20 towards Broadway
+					</p>
+				</div>
 			</div>
 		</section>
 
-		<section class="page-section">
-			<p>If you are curious to hear more about our work and process, interested in hiring us for your project, or simply want to ask about job availability, please do not hesitate to contact us.</p>
-			<p class="h2 future-A"><a href="#talk-to-us" class="text-color-link">talk to us</a></p>
-			<p class="h2 hug future-A"><a href="#hire-us" class="text-color-link">hire us</a></p>
-			<p class="h2 hug future-A"><a href="#join-us" class="text-color-link">join us</a></p>
+		<section class="page-section outer-container">
+			<div class="inner-container">
+				<p>If you are curious to hear more about our work and process, interested in hiring us for your project, or simply want to ask about job availability, please do not hesitate to contact us.</p>
+				<p class="h2 future-A"><a href="#talk-to-us" class="text-color-link">talk to us</a></p>
+				<p class="h2 hug future-A"><a href="#hire-us" class="text-color-link">hire us</a></p>
+				<p class="h2 hug future-A"><a href="#join-us" class="text-color-link">join us</a></p>
+			</div>
 		</section>
 
 	<?php endwhile; // end of the loop. ?>
