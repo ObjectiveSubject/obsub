41304b6b236c07db5f5f1a88f46a230e42e6d4e9
Kirk Pettinga <kirk.pettinga@gmail.com>
2016-01-21 15:21:58 +0700

    header stuff

--- js/src/main-menu.js
+++ js/src/main-menu.js
@@ -8,16 +8,12 @@
 		e.preventDefault();
 		var $siteHeader = $('.site-header'),
 			$mainMenu = $('.main-menu'),
-			// $wordmarkLight = $('.wordmark.light'),
-			// $wordmarkDark = $('.wordmark.dark'),
 			animationSequence;
 
 		if ( $siteHeader.hasClass('main-menu-active') ) {
 			$siteHeader.removeClass('main-menu-active');
 			animationSequence = [
 				{ e: $(".main-menu > li"),	p: { translateY: "20px" }, 	o: { duration: 200, stagger: 200 } },
-				// { e: $wordmarkDark, 		p: "fadeIn", 		o: { duration: 300, display: "block" } },
-				// { e: $wordmarkLight,		p: "fadeOut", 		o: { duration: 300, display: "block", sequenceQueue: false } },
 				{ e: $mainMenu, 			p: "fadeOut" , 		o: { duration: 300, sequenceQueue: false } },
 				{ e: $('.main-menu-scrim'), p: "fadeOut" , 		o: { duration: 300, sequenceQueue: false } }
 			];
@@ -30,8 +26,6 @@
 			animationSequence = [
 				{ e: $('.main-menu-scrim'), p: "fadeIn", 		o: { duration: 300 } },
 			    { e: $mainMenu, 			p: "fadeIn", 		o: { duration: 300, sequenceQueue: false } },
-			    // { e: $wordmarkDark, 		p: "fadeOut", 		o: { duration: 300, sequenceQueue: false } },
-			    // { e: $wordmarkLight, 		p: "fadeIn", 		o: { duration: 300, sequenceQueue: false } },
 			    { e: $(".main-menu > li"), 	p: { translateY: "0" },	o: { duration: 200, stagger: 200 } },
 			];
 			$.Velocity.RunSequence(animationSequence);
