6092c2418004b4afe5a32dedf19e7e7e8826dfe4
Kirk Pettinga <kirk.pettinga@gmail.com>
2016-02-04 11:40:22 +0700

    links in arnhem; #25

--- front-page.php
+++ front-page.php
@@ -71,9 +71,9 @@ get_header(); ?>
 						<div><img src="<?php echo os_path('logo-nytimes.png', 'home'); ?>" /></div>
 						<p class="push">We’ve worked with the Times to codify its design tools into a cogent set of rules and principles that can be applied by marketers, designers and advertisers who partner with The New York Times Brand. This brand book is leveraged both inside the organization and with outside groups to make every communication feel ‘Timesian.’</p>
 						<p class="push">We created an award-winning website for the Times’ travel product, Times Journeys, and continue to offer strategic and creative services to support the new ways one can Travel with The New York Times.</p>
-						<p class="push future-A"><a href="http://nytimes.com/timesjourneys" target="_blank">nytimes.com/timesjourneys</a></p>
+						<p class="push"><a href="http://nytimes.com/timesjourneys" target="_blank"><em>nytimes.com/timesjourneys</em></a></p>
 						<p class="push">More recently, alongside our partners at Sotheby’s Institute of Art, we designed a digital interface for the new School of The New York Times, nytEDU. We addressed content strategy, including sitemaps, wireframes and recommendations on program naming, and visual design to educate users on ways to Learn with The New York Times.</p>
-						<p class="push future-A"><a href="http://nytedu.com" target="_blank">nytedu.com</a></p>
+						<p class="push"><a href="http://nytedu.com" target="_blank"><em>nytedu.com</em></a></p>
 					</div>
 				</li>
 				<li class="tile push-triple layout-image-left">
@@ -106,21 +106,21 @@ get_header(); ?>
 					<div class="tile-content push">
 						<div><img src="<?php echo os_path('logo-bam.png', 'home'); ?>" /></div>
 						<p class="push">The Brooklyn Academy of Music is an ambassador of cutting edge arts and music. Our design research found that would-be visitors struggled to find their way through the BAM campus and its many venues. We designed a visitor experience that seamlessly integrates BAM’s existing web platform and visual brand to help guests plan an evening around a BAM outing, from pre- dinner drinks to their subway ride home.</p>
-						<p class="push future-A"><a href="http://bam.org/visit" target="_blank">bam.org/visit</a></p>
+						<p class="push"><a href="http://bam.org/visit" target="_blank"><em>bam.org/visit</em></a></p>
 					</div>
 				</li>
 				<li class="tile push-triple layout-half left">
 					<div class="tile-content push">
 						<div><img src="<?php echo os_path('logo-orpheus.png', 'home'); ?>" /></div>
 						<p class="push">Performing without a conductor, Orpheus Chamber Orchestra is one of the world’s foremost chamber orchestras. Its digital presence needed to both present its distinctive story as well as accomplish key business goals in ticket and subscription sales. The final solution layers various elements,bringing the vitality and emotion of the music to every user interaction.</p>
-						<p class="push future-A"><a href="http://orpheusnyc.org" target="_blank">orpheusnyc.org</a></p>
+						<p class="push"><a href="http://orpheusnyc.org" target="_blank"><em>orpheusnyc.org</em></a></p>
 					</div>
 				</li>
 				<li class="tile push-triple layout-half right">
 					<div class="tile-content push">
 						<div><img src="<?php echo os_path('logo-nbr.png', 'home'); ?>" /></div>
 						<p class="push">Since 1909, the National Board of Review has been New York City’s premier film awards organization, dedicated to the celebration and advancement of excellence in cinema. We were inspired by typography in early publications to develop a visual identity that is appropriately modern. Our work extends through a suite of materials, including a website, that recall the velvety and clubby feel of a projection room.</p>
-						<p class="push future-A"><a href="http://nationalboardofreview.org">nationalboardofreview.org</a></p>
+						<p class="push"><a href="http://nationalboardofreview.org"><em>nationalboardofreview.org</em></a></p>
 					</div>
 				</li>
 				<li class="tile push-triple layout-image-left">
@@ -128,7 +128,7 @@ get_header(); ?>
 					<div class="tile-content push">
 						<div><img src="<?php echo os_path('logo-gibney.png', 'home'); ?>" /></div>
 						<p class="push">In collaboration with Gina Gibney, Objective Subject helped the growing dance organization define its brand positioning and develop messaging to best communicate the breadth and depth of its programming as well as its future vision. Our work included designing and building a new website, and designing visual communications worthy of any major arts organization in NYC.</p>
-						<p class="push future-A"><a href="http://gibneydance.org" target="_blank">gibneydance.org</a></p>
+						<p class="push"><a href="http://gibneydance.org" target="_blank"><em>gibneydance.org</em></a></p>
 					</div>
 				</li>
 			</ul>
@@ -140,7 +140,7 @@ get_header(); ?>
 						<div><img src="<?php echo os_path('logo-guittard.png', 'home'); ?>" /></div>
 						<p class="push">After nearly 150 years of making the best chocolate in the world, the Guittard Chocolate Company was looking for a way to unify its offering and leverage its reputation for quality and integrity across its branded communications.</p>
 						<p class="push">Our team conducted in-depth research with employees, partners, and customers, to identify key values and personality traits. We recommended the adoption of a master brand strategy, centered around the promise of 'Chocolate for Experts.' The corporate wordmark is derived from the founder Étienne Guittard’s signature. We leveraged the company’s navy blue for the master brand, supported by a palette of subtle, earthy and bright colors to denote the quality of the natural ingredients.</p>
-						<p class="push future-A"><a href="http://guittard.com" target="_blank">guittard.com</a></p>
+						<p class="push"><a href="http://guittard.com" target="_blank"><em>guittard.com</em></a></p>
 					</div>
 				</li>
 				<li class="tile push-triple layout-half right">
@@ -149,7 +149,7 @@ get_header(); ?>
 						<p class="push">Carr McClellan is a full-service boutique firm with a strong track record for its clients, located throughout Northern California.</p>
 						<p class="push">The new positioning is captured in the brand strategy and reinforced by the new look: Carr McClellan is committed to advancing clients’ business goals with an approach that is effective, down-to-earth, and enthusiastic.</p>
 						<p class="push">The visual system we designed perfectly balances a modern approach, while still referencing the firm's strong heritage and persistent stability amidst a volatile industry and economy.</p>
-						<p class="push future-A"><a href="http://carrmclellan.org" target="_blank">carrmclellan.org</a></p>
+						<p class="push"><a href="http://carrmclellan.org" target="_blank"><em>carrmclellan.org</em></a></p>
 					</div>
 				</li>
 				<li class="tile push-triple layout-half left">
@@ -158,7 +158,7 @@ get_header(); ?>
 						<p class="push">An undisputed leader in educational travel for nearly 50 years, WorldStrides offers a wide range of traveler experiences from elementary school trips to full-scale group itineraries for graduate students in MBA programs around the world.</p>
 						<p class="push">Our work involved robust content strategy to bring harmony across a disparate set of metadata. Our research and analysis determined that the individual trip, or itinerary, ought to be the key node around which all content is organized. We created a series of reusable content modules which are shared amongst itineraries, simplifying site maintenance, while creating unique and immersive experiences for the user.</p>
 						<p class="push">Since launching in November 2015, the results have been impressive, with a 50% jump in lead generation.</p>
-						<p class="push future-A"><a href="http://worldstrides.com" target="_blank">worldstrides.com</a></p>
+						<p class="push"><a href="http://worldstrides.com" target="_blank"><em>worldstrides.com</em></a></p>
 					</div>
 				</li>
 			</ul>
