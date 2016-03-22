2725bbd13b1324026a0810a73898d66772e11db3
Kirk Pettinga <kirk.pettinga@gmail.com>
2016-03-22 15:55:34 +0700

    add cfr case study

---
+++ content-cfr.php
@@ -0,0 +1,104 @@
+<?php
+	$color = get_field('case_study_color');
+?>
+
+<section id="opportunity" class="page-section">
+	<div class="outer-container">
+		<div class="inner-container">
+			<h2 class="future-A">opportunity</h2>
+			<p>Founded in 1921, Council on Foreign Relations is an independent, non-partisan membership organization, think tank, and publisher.</p>
+			<p>With over 340,000 unique visitors monthly, CFR.org is a hub for the organization’s published work, original interactive media, and institutional information.</p>
+		</div>
+	</div>
+
+	<figure class="alignnone">
+		<div class="parallax-container">
+			<blockquote class="parallax-node" data-speed-modifier="0.5" style="background-color:<?php echo $color; ?>; color: white; ">
+				<p><span class="quote">&ldquo;</span>A Quote<span class="quote">&rdquo;</span></p>
+			</blockquote>
+			<img src="http://placehold.it/1200x600/dddddd/ffffff" width="1200" height="600">
+		</div>
+	</figure>
+
+</section>
+
+<section id="process" class="page-section">
+
+	<div class="outer-container">
+		<div class="inner-container">
+			<h2 class="future-A push-triple">process</h2>
+			<p>In August 2008, CFR debuted a new visual identity, created by <a href="http://landor.com/" class="cs-body-link" target="_blank">Landor Associates</a>, one of the world’s leading strategic brand consulting firms. Shortly thereafter, Objective Subject realized the comprehensive communication system across all physical and virtual applications.</p>
+			<p>After an intensive organization-wide audit of material uses and needs, Objective Subject refined brand guidelines to govern all platforms including printed publications, online material, and physical touch-points.</p>
+		</div>
+	</div>
+
+	<figure class="aligncenter">
+		<img src="http://placehold.it/1100x600/dddddd/ffffff" srcset="" width="1100" height="600" />
+		<div class="outer-container">
+			<figcaption class="inner-container">Council on Foreign Relations business collateral</figcaption>
+		</div>
+	</figure>
+
+	<div class="outer-container">
+		<div class="inner-container">
+			<p>CFR has a diverse portfolio of publications that disseminate critical thinking on pertinent foreign-policy issues. We transformed their communications from low-quality, laser-printed stationery and print materials to one consistent suite of custom, on-brand collateral reflecting the quality of CFR’s work. All elements were produced with 100% post-consumer waste & FSC certified paper—all for less than previous printing costs.</p>
+		</div>
+	</div>
+
+	<figure class="aligncenter">
+		<img src="http://placehold.it/1100x600/dddddd/ffffff" srcset="" width="1100" height="600" />
+		<div class="outer-container">
+			<figcaption class="inner-container">Council on Foreign Relations Task Force Reports 66–73</figcaption>
+		</div>
+	</figure>
+
+	<figure class="alignnone">
+		<img src="http://placehold.it/1400x600/dddddd/ffffff" srcset="" width="1400" height="600" />
+		<div class="outer-container">
+			<figcaption class="inner-container">CFR's The Chronicle</figcaption>
+		</div>
+	</figure>
+
+</section>
+
+<section id="solution" class="page-section" style="padding-bottom: 200px;">
+
+	<div class="outer-container">
+		<div class="inner-container">
+			<h2 class="future-A">solution</h2>
+			<p>We worked closely with their in-house web development team to overhaul the design of the site to create a straight-forward, elegant, and on-brand digital experience.</p>
+			<p>The CFR guidelines are a living document that adapts to the ongoing needs of the brand. We are the gatekeepers of the brand and amend this document to adjust to evolving opportunities and requirements.</p>
+		</div>
+	</div>
+
+	<figure class="os-slideshow-container">
+		<div id="ap-environment-dots"></div>
+		<ul class="list-unstyled os-slideshow center-mode" data-slick='{ "prevArrow": "#ap-environment-nav .prev", "nextArrow": "#ap-environment-nav .next", "appendDots": "#ap-environment-dots" }'>
+			<li>
+				<img src="<?php echo os_path('AP-slideshow-1.png', 'ap'); ?>" srcset="<?php echo os_path('AP-slideshow-1.png', 'ap'); ?> 1x, <?php echo os_path('AP-slideshow-1.png', 'ap'); ?> 2x" width="850" height="567">
+				<figcaption>Caption</figcaption>
+			</li>
+			<li>
+				<img src="<?php echo os_path('AP-slideshow-2.png', 'ap'); ?>" srcset="<?php echo os_path('AP-slideshow-2.png', 'ap'); ?> 1x, <?php echo os_path('AP-slideshow-2-@2x.png', 'ap'); ?> 2x" width="850" height="567">
+				<figcaption>Caption</figcaption>
+			</li>
+			<li>
+				<img src="<?php echo os_path('AP-slideshow-3.png', 'ap'); ?>" srcset="<?php echo os_path('AP-slideshow-3.png', 'ap'); ?> 1x, <?php echo os_path('AP-slideshow-3-@2x.png', 'ap'); ?> 2x" width="850" height="567">
+				<figcaption>Caption</figcaption>
+			</li>
+		</ul>
+		<?php os_slide_nav("ap-environment-nav"); ?>
+	</figure>
+
+	<div class="outer-container">
+		<div class="inner-container">
+			<h2 class="future-A push-triple">ongoing engagement</h2>
+			<p>We are proud to continually partner with CFR to help realize key brand communications and strategy since our initial engagement in 2008.</p>
+		</div>
+	</div>
+
+	<div class="outer-container">
+		<p class="h2 future-A inner-container"><a href="http://cfr.org/" class="cta-link cs-body-link" target="_blank">cfr.org <span class="icon arnhem rotate-cc-45">&rarr;</span></a></p>
+	</div>
+
+</section>
