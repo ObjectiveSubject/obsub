edccd6b00106c751d22ab43e97729642356267ff
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-05-31 14:03:35 +0700

    move analytics to <head>

--- footer.php
+++ footer.php
@@ -31,17 +31,7 @@
 
 </div><!-- #page -->
 
-<?php if ( WP_DEBUG === false ) : ?>
-	<!-- Google Analytics -->
-	<script async>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-2792624-1', 'auto');
-	  ga('send', 'page view')
-	</script>
-<?php else : ?>
+<?php if ( defined('WP_DEBUG') && WP_DEBUG === TRUE ) : ?>
 	<div class="media-size"></div>
 <?php endif; ?>
 
--- header.php
+++ header.php
@@ -17,6 +17,17 @@
 <?php if ( is_page( 'contact' ) || is_singular( 'case_study' ) ) : ?>
 	<script src='https://www.google.com/recaptcha/api.js' async></script>
 <?php endif ;?>
+
+<!-- Google Analytics -->
+<script async>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-2792624-1', 'auto');
+	ga('send', 'page view')
+</script>
+
 </head>
 
 <body <?php body_class(); ?> >
