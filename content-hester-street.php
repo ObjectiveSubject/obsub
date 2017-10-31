316dd313ccf402c93e23abeee0cd742359c42a0c
Amir Tawfik <amirtawfik@Houman-iMac.fios-router.home>
2017-10-31 13:03:20 -0400

    add HesterStreet

--- content-hester-street.php
+++ content-hester-street.php
@@ -35,7 +35,7 @@
 			</div>
 	</div>
 
-	<figure class="alignnone">
+	<figure class="alignnone push-double">
 		<div class="parallax-container">
 			<blockquote class="parallax-node" data-speed-modifier="0.5">
 				<p><span class="quote">&ldquo;</span>[Quote from Hester Street]<span class="quote">&rdquo;</span><br/><small>&mdash;Betsy MacLean, Executive Director</small></p>
@@ -46,7 +46,7 @@
 	</figure>
 </section>
 
-<section id="process" class="page-section ">
+<section id="process" class="page-section hug ">
 
 	<div class="outer-container">
 		<h2 class="future-A inner-container push-triple">process</h2>
@@ -55,119 +55,85 @@
 			<p>Our designers iteratively developed a new identity system and an accessible, streamlined website, and our strategists crafted and tested a multi-level messaging platform, all inspired by our research findings.</p>
 		</div>
 	</div>
-	<figure class="aligncenter" style="background-color: white;">
+	<figure class="alignnone">
 		<img src="<?php echo os_path('HS-Old_hester.jpg', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Old_hester.jpg', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Old_hester@2x.jpg', 'hesterstreet'); ?> 2x"/>
-		<!-- <figcaption>explanatory caption</figcaption> -->
 	</figure>
-
-	<figure class="oakland-tree">
-		<div class="labels outer-container">
-			<div class="inner-container">
-				<em class="pull-left text-muted" style="width: 50%; color: #376FFF; ">Before</em>
-				<em class="pull-right text-muted text-right" style="width: 50%; color: #376FFF; ">After</em>
-			</div>
-		</div>
-		<div class="slider-row oakland-tree-large">
-			<img src="<?php echo os_path('CoO-Tree2.svg', 'oakland'); ?>" class="large top" width="640" height="438" />
-			<img src="<?php echo os_path('CoO-Tree1.svg', 'oakland'); ?>" class="large bottom" width="640" height="438" />
-			<div class="slider">
-				<div class="slider-track"></div>
-				<div class="slider-thumb"></div>
-				<input type="range" name="tree-slider-lg" class="slider-input tree-slider-lg" value="50" min="0" max="100">
-			</div>
-		</div>
-		<div class="slider-row oakland-tree-small">
-			<img src="<?php echo os_path('CoO-Tree2.svg', 'oakland'); ?>" class="small top" width="116" height="119" />
-			<img src="<?php echo os_path('CoO-Tree1.svg', 'oakland'); ?>" class="small bottom" width="116" height="119" />
-			<div class="slider">
-				<div class="slider-track"></div>
-				<div class="slider-thumb"></div>
-				<input type="range" name="tree-slider-sm" class="slider-input tree-slider-sm" value="50" min="0" max="100">
-			</div>
-		</div>
-	</figure>
-
 	<div class="outer-container">
-		<figcaption class="inner-container">Oakland’s tree</figcaption>
-		<p class="inner-container push-triple">As we defined the color scheme, we wanted to help Oakland break through the clutter of commercial activity by giving it a clear and bold look. We paired a dominant color, an energetic green hue, to a rich range of complimentary hues, and created a typographic system that emphasizes clarity and lack of ornament.</p>
-	</div>
-
-	<figure class="aligncenter hug">
-		<img src="<?php echo os_path('CoO-Brochure-lowsrc.png', 'oakland') ?>" class="lazy-load" data-src="<?php echo os_path('CoO-Brochure.png', 'oakland'); ?>" data-src2x="<?php echo os_path('CoO-Brochure-@2x.png', 'oakland'); ?>" width="1025" height="690" />
-		<div class="outer-container">
-			<figcaption class="inner-container">City of Oakland Brochures</figcaption>
-		</div>
-	</figure>
-
-	<div class="outer-container push-triple">
-		<p class="inner-container">The City should be able to modulate its expression without always employing a blunt use of the logo. We created a pattern that, while related to the tree logo, grows it into a full root system, as complex and rich as the communities and network that support the city. This pattern can be applied to elements in overt or subtle ways, based on the context.</p>
+		<figcaption class="inner-container">Norfolk and Hester Street around 1898.</figcaption>
 	</div>
 </section>
 
-<section class="page-section " style="background-color: #E0F3F2; padding-bottom: 100px">
-	<figure class="alignnone hug oakland-pattern"></figure>
-	<div class="outer-container">
-		<figcaption class="inner-container">Root system</figcaption>
-	</div>
-</section>
-
-<section id="solution" class="page-section " style="background-color: #E0F3F2; padding-bottom: 200px">
+<section id="solution" class="page-section">
 
 	<div class="outer-container">
 		<div class="inner-container">
 			<h2 class="future-A">solution</h2>
+			<p>We renamed the organization to simply, ‘Hester Street’, removing the Collaborative moniker, which did not add much and even confused perceptions of the organization.</p>
 		</div>
+
+		<figure class="aligncenter">
+			<svg class="hs-wordmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 510.88 177.94">
+			  <title>HS-wordmark</title>
+			      <path d="M61.07,147.56H43.74V96.49H17.33v51.07H0V30.38H17.33V81.45H43.74V30.38H61.07Zm63.87-15v15h-50V30.38h48.85v15H92.26v36h28.88v15H92.26v36Zm8.8-13.35v-9.74H151.2l.19,8.59c0,12,4,18.32,12.21,18.32,7.76,0,11.39-5.45,11.39-14.19v-4.29c0-9.24-1.16-13.86-10.23-19.81l-14-8.91C139,81.54,134.89,72.79,134.89,60.74V54.8c0-17,11.39-27.23,28.72-27.23,18.48,0,29,8.09,29,28.72V65l-17.33-.38V57.94c0-10.89-3.63-16.5-11.72-16.5-7.1,0-10.89,5.12-10.89,12.21v5.61c0,7.59,2.81,12.54,9.74,16.84l14.36,9.24C190,93.75,193,102.83,193,115.54v6.27c0,18-10.07,28.55-29.54,28.55C144.3,150.36,133.74,139.31,133.74,119.17ZM263.49,45.56H240.71v102h-17v-102H200.77V30.38h62.72Zm61.56,87v15H275V30.38h50v15H292.37V80.07h28.88v15H292.37v37.41ZM446.4,104.74c-12,0-18.85-7.19-18.85-19.72v-7.2l13.36,0v6.57c0,9.15,4.06,9.15,5.59,9.15,1.26,0,5.11,0,5.11-6.75v-2.5c0-4.94-.45-7.11-5.27-10.26l-8.16-5.18c-7-4.52-9.95-9.85-9.95-17.84V47.5c0-10.57,7.17-17.41,18.27-17.41,12.25,0,18.47,6.15,18.47,18.27V55l-13.17-.06V49.33c0-7.17-2.83-8.09-5.3-8.09-4.35,0-4.82,3.91-4.82,5.59V50.1c0,3.92,1.39,6.31,5,8.51L455,64c8.41,5.36,10.13,11.43,10.13,18.9v3.66C465.15,98.12,458.32,104.74,446.4,104.74Zm51.2-.78h-13V42.29H471.25V30.38h39.63V42.29H497.6Zm-131.31-10H356.22l0,53.64H338.91V30.38h27.56c21.46,0,30.2,10.89,30.37,30V67c0,14.52-5.49,23.28-14.57,27.24l17.54,53.3H381.5Zm-.8-13.78c9.74,0,13.53-1.27,13.53-14.63V57.81c0-13.33-3.78-14.73-13.7-14.73h-9.08V80.14ZM510.88,0H0V15.19H510.88Zm0,162.75H0v15.19H510.88Z"/>
+			</svg>
+		</figure>
+
+			<div class="inner-container">
+				<figcaption>Hester Street wordmark</figcaption>
+			</div>
+
+				<p class="inner-container">We complemented the new brand with a strapline that simultaneously expresses the brand promise, a sense of place, and the ownership of the community for their built environment: ‘Where the people shape their city.’ A short descriptor adds further detail for written communications, and an elevator pitch provides all staff and supporters with a go-to phrase for use when engaging with other people – an integral part of their process.</p>
 
 	</div>
 
-	<figure class="os-slideshow-container">
-		<div id="oakland-website-dots"></div>
-		<ul class="list-unstyled os-slideshow center-mode" data-slick='{ "prevArrow": "#oakland-website-nav .prev", "nextArrow": "#oakland-website-nav .next", "appendDots": "#oakland-website-dots" }'>
-			<li class="browser">
-				<div class="browser-bar"></div>
-				<div class="browser-window">
-					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-Homepage-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-Homepage-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-Homepage-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
-					</div>
-				</div>
-				<figcaption>City of Oakland Homepage</figcaption>
-			</li>
-			<li class="browser">
-				<div class="browser-bar"></div>
-				<div class="browser-window">
-					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-Landing-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-Landing-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-Landing-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
-					</div>
-				</div>
-				<figcaption>Landing Page</figcaption>
-			</li>
-			<li class="browser">
-				<div class="browser-bar"></div>
-				<div class="browser-window">
-					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-Parks-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-Parks-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-Parks-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
-					</div>
-				</div>
-				<figcaption>Parks and Recreation</figcaption>
-			</li>
-			<li class="browser">
-				<div class="browser-bar"></div>
-				<div class="browser-window">
-					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-Terminal-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-Terminal-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-Terminal-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
-					</div>
-				</div>
-				<figcaption>Terminal page</figcaption>
-			</li>
-		</ul>
-		<?php os_slide_nav("oakland-website-nav"); ?>
+	<figure class="alignnone push-triple" style="background-color: #000;height:72vw;">
+		<div class="outer-container gt-america"
+				 style="color: white;
+						    height: 100%;
+						    line-height: .9em;
+						    font-size: 10vw;
+						    max-width: 99vw;
+
+						    padding-top: 22vw;">
+				WHERE THE PEOPLE SHAPE <br>
+				<span class="txt-typing"
+     		data-period="2000"
+     		data-rotate="The city;NYC's culture;Cypress Hill;East Harlem;The Bronx"></span>
+		</div>
 	</figure>
+	<div class="outer-container">
+		<figcaption class="inner-container">Hester Street’s flexible strapline</figcaption>
+	</div>
 
 	<div class="outer-container">
-		<div class="inner-container">
-			<p class="h2 future-A"><a href="http://beta.oaklandca.gov/" class="cta-link" target="_blank">beta.oaklandca.gov <span class="icon arnhem rotate-cc-45">&rarr;</span></a></p>
-			<p>In this same spirit of open development, the portfolio library is publicly available, along with a design toolkit created to support designers, agencies and civil servants in creating material to fit the new visual identity.</p>
-		</div>
+		<p class="inner-container">Hester Street’s communication materials got a fresh new look and feel. A new logo was inspired by the visual language of the street; the structure and layout of materials are drawn from the vertical lines of the urban landscape, a true testament to Hester Street’s roots in supporting equitable urban space.</p>
+	</div>
+
+	<figure class="aligncenter">
+		<img src="<?php echo os_path('HS-Stationery-Mockup.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Stationery-Mockup.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Stationery-Mockup@2x.png', 'hesterstreet'); ?> 2x"/>
+	</figure>
+	<div class="outer-container">
+		<figcaption class="inner-container">Hester Street Stationery</figcaption>
+	</div>
+
+	<figure class="aligncenter">
+		<img src="<?php echo os_path('HS-Benefit-Mockup.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Benefit-Mockup.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Benefit-Mockup@2x.png', 'hesterstreet'); ?> 2x"/>
+	</figure>
+	<div class="outer-container">
+		<figcaption class="inner-container">Hester Street 15 Year Benefit Invite</figcaption>
+	</div>
+
+	<div class="outer-container">
+		<p class="inner-container">We paired a warm and vibrant color palette with a clean and clear black-and-white base, and a bold and modern typeface, GT America. Stylized photography demonstrates the focus of Hester Street’s work – the human beings within the city.</p>
+	</div>
+
+	<figure class="aligncenter">
+		<img src="<?php echo os_path('HS-Brand-Guidelines-Mockup.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Brand-Guidelines-Mockup.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Brand-Guidelines-Mockup@2x.png', 'hesterstreet'); ?> 2x"/>
+	</figure>
+	<div class="outer-container">
+		<figcaption class="inner-container">Hester Street Brand Guidelines</figcaption>
+
+		<p class="inner-container">The new website is structured to reflect Hester Street’s work and provide a rolling record of events and projects, streamlining their documenting process. </p>
 	</div>
 
 	<figure class="os-slideshow-container">
@@ -177,28 +143,37 @@
 				<div class="browser-bar"></div>
 				<div class="browser-window">
 					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-PatternPortfolio-1-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-PatternPortfolio-1-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-PatternPortfolio-1-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
+						<img src="<?php echo os_path('HS-Project-Hub.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Project-Hub.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Project-Hub-@2x.png', 'hesterstreet'); ?> 2x" width="850" height="780">
 					</div>
 				</div>
-				<figcaption>City of Oakland Design Toolkit</figcaption>
+				<figcaption>Hester Street Project Hub</figcaption>
 			</li>
 			<li class="browser">
 				<div class="browser-bar"></div>
 				<div class="browser-window">
 					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-PatternPortfolio-2-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-PatternPortfolio-2-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-PatternPortfolio-2-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
+						<img src="<?php echo os_path('HS-Project-Page.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Project-Page.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Project-Page-@2x.png', 'hesterstreet'); ?> 2x" width="850" height="780">
 					</div>
 				</div>
-				<figcaption>Toolkit – Visual Identity</figcaption>
+				<figcaption>Toolkit - Project Page</figcaption>
 			</li>
 			<li class="browser">
 				<div class="browser-bar"></div>
 				<div class="browser-window">
 					<div class="browser-inner">
-						<img src="<?php echo os_path('CoO-PatternPortfolio-3-min.png', 'oakland'); ?>" srcset="<?php echo os_path('CoO-PatternPortfolio-3-min.png', 'oakland'); ?> 1x, <?php echo os_path('CoO-PatternPortfolio-3-@2x-min.png', 'oakland'); ?> 2x" width="850" height="780">
+						<img src="<?php echo os_path('HS-Team-Hub.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Team-Hub.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Team-Hub-@2x.png', 'hesterstreet'); ?> 2x" width="850" height="780">
 					</div>
 				</div>
-				<figcaption>Toolkit – Pattern Portfolio</figcaption>
+				<figcaption>Toolkit – Team Hub</figcaption>
+			</li>
+			<li class="browser">
+				<div class="browser-bar"></div>
+				<div class="browser-window">
+					<div class="browser-inner">
+						<img src="<?php echo os_path('HS-Team-Member.png', 'hesterstreet'); ?>" srcset="<?php echo os_path('HS-Team-Member.png', 'hesterstreet'); ?> 1x, <?php echo os_path('HS-Team-Member-@2x.png', 'hesterstreet'); ?> 2x" width="850" height="780">
+					</div>
+				</div>
+				<figcaption>Toolkit – Team Member</figcaption>
 			</li>
 		</ul>
 		<?php os_slide_nav("oakland-toolkit-nav"); ?>
@@ -206,9 +181,12 @@
 
 	<div class="outer-container">
 		<div class="inner-container">
-			<p>We continue to support the City as it improves the quality of its visual communication.</p>
-			<p class="h2 future-A"><a href="http://design.oaklandca.gov/" class="cta-link" target="_blank">design.oaklandca.gov <span class="icon arnhem rotate-cc-45">&rarr;</span></a></p>
+			<p>The team and their skills are prominent on the site, helping people to understand their complex but holistic offer.</p>
+			<p class="h2 future-A"><a href="http://hesterstreet.org/" class="cta-link" target="_blank">hesterstreet.org <span class="icon arnhem rotate-cc-45">&rarr;</span></a></p>
 		</div>
+	</div>
+	<div class="outer-container push-double">
+
 	</div>
 
 </section>
