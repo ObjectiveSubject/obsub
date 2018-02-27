24a20271be14d77902e96b77fd03a68e7a631502
a-tawfik <taamer@gmail.com>
2018-02-27 09:24:28 -0500

    atil case study

---
+++ content-atil.php
@@ -0,0 +1,285 @@
+<section id="opportunity" class="page-section">
+	<div class="outer-container bleed">
+		<div class="inner-container">
+			<h2 class="future-A">opportunity</h2>
+			<p>Fresh off our brand refresh, TransitCenter came to us with a concept for their next research topic, which encouraged politicians and elected officials to invest in public transit and institute policies that support ridership on those investments by building neighborhoods designed for people, not cars. TransitCenter explained that many city leaders want to support transit-friendly neighborhoods for their constituents, but think it’s out of their jurisdiction or are overwhelmed by how to do it.</p>
+		</div>
+
+	</div>
+
+	<figure class="alignnone">
+		<div class="parallax-container">
+			<blockquote class="parallax-node" data-speed-modifier="0.5">
+				<p class="bleed"><span class="quote">&ldquo;</span>Leaders who link transportation accomplishments to broad goals can improve life for their citizens while winning public acclaim.<span class="quote">&rdquo;</span><br/><small>&mdash; All Transportation is Local</small></p>
+			</blockquote>
+			<img src="<?php echo os_path('LA-Mayor.jpg', 'atil') ?>" srcset="<?php echo os_path('LA-Mayor.jpg', 'atil') ?> 1x, <?php echo os_path('LA-Mayor@2x.jpg', 'atil') ?> 2x" width="1600" height="1008" alt="Los Angeles Mayor Eric Garcetti at the Measure M victory press conference">
+		</div>
+		<figcaption class="outer-container"><div class="inner-container">Los Angeles Mayor Eric Garcetti at the Measure M victory press conference</div></figcaption>
+	</figure>
+
+	<div class="outer-container bleed push-triple">
+		<div class="inner-container">
+			<p>It became clear that there was an opportunity to incorporate the larger landscape of urban policies that contribute to the lively, walkable neighborhoods conducive to transit usage. Furthermore, there was an opportunity to render dense policy matters in a more compelling and digestible way.</p>
+		</div>
+	</div>
+</section>
+
+<section id="process" class="page-section push-triple">
+
+	<div class="outer-container bleed ">
+		<div class="inner-container">
+			<h2 class="future-A">process</h2>
+			<p>The original draft publication was a persuasive explanation of how local policies like parking requirements hinder transit infrastructure in American cities by focusing development around the needs of the automobile. But as transit policy nerds ourselves, we understood that parking requirements were just one of many ways that public transit projects are sidelined in cities, even when local elected officials are eager to improve or introduce them. Recognizing the complex, interconnected policies and practices that hamper transit development in American cities, we went back to TransitCenter with an idea.</p>
+			<p>We sketched out a visual manifestation of the breadth of actions that can impact neighborhood development—from zoning policy to building form to quick-build, walkable (and bikeable) street design—and the resulting urban landscape. Starting with the typical American city’s development optimized for car use, we created a series of sketches that layered the effects of policy changes that could enable more vibrant, mixed-use, and transit-friendly development to cities.</p>
+		</div>
+	</div>
+
+	<figure class="os-slideshow-container">
+		<div id="bps-billboard-dots"></div>
+		<ul class="list-unstyled os-slideshow center-mode" data-slick='{ "prevArrow": "#bps-billboard-nav .prev", "nextArrow": "#bps-billboard-nav .next", "appendDots": "#bps-billboard-dots" }'>
+			<li>
+				<img src="<?php echo os_path('placeholder900x600.jpg', 'atil'); ?>" srcset="<?php echo os_path('placeholder900x600.jpg', 'atil'); ?> 1x, <?php echo os_path('placeholder900x600@2x.jpg', 'atil'); ?> 2x" width="850" height="566" />
+				<figcaption class="outer-container">TBD</figcaption>
+			</li>
+			<li>
+				<img src="<?php echo os_path('placeholder900x600.jpg', 'atil'); ?>" srcset="<?php echo os_path('placeholder900x600.jpg', 'atil'); ?> 1x, <?php echo os_path('placeholder900x600@2x.jpg', 'atil'); ?> 2x" width="850" height="566" />
+				<figcaption class="outer-container">TBD</figcaption>
+			</li>
+			<li>
+				<img src="<?php echo os_path('placeholder900x600.jpg', 'atil'); ?>" srcset="<?php echo os_path('placeholder900x600.jpg', 'atil'); ?> 1x, <?php echo os_path('placeholder900x600@2x.jpg', 'atil'); ?> 2x" width="850" height="566" />
+				<figcaption class="outer-container">TBD</figcaption>
+			</li>
+		</ul>
+		<?php os_slide_nav("bps-billboard-nav"); ?>
+	</figure>
+
+	<div class="outer-container bleed push-triple">
+		<div class="inner-container">
+			<p>After a series of robust and fruitful conversations, the TransitCenter team then applied their in-depth knowledge, research, and experience to flesh out the content across the framework.</p>
+		</div>
+	</div>
+
+	<blockquote class="inverted" data-speed-modifier="0.5">
+		<p class="bleed"><span class="quote">&ldquo;</span>Quote from TransitCenter<span class="quote">&rdquo;</span><br/><small>&mdash;  John Smith, TransitCenter Employee</small></p>
+	</blockquote>
+
+
+</section>
+
+<section id="solution" class="page-section ">
+
+	<div class="outer-container">
+		<div class="inner-container">
+			<h2 class="future-A">solution</h2>
+			<p>Always bearing in mind the target audience—civic leaders—and the possibility of shifting political environments, we developed a concept for a handy toolkit that would be equally useful, compelling, and adaptable to new circumstances.</p>
+		</div>
+	</div>
+
+	<figure class="aligncenter ">
+		<div class="outer-container">
+			<ul class="cycle-images">
+				<li class="img-1">
+					<img  src="<?php echo os_path('drawing-1.png', 'atil') ?>" width="1212" height="978" srcset="<?php echo os_path('drawing-1.png', 'atil') ?> 1x, <?php echo os_path('drawing-1@2x.png', 'atil') ?> 2x" width="1600" height="1008">
+				</li>
+				<li class="img-2">
+					<img src="<?php echo os_path('drawing-2.png', 'atil') ?>" width="1212" height="978" srcset="<?php echo os_path('drawing-2.png', 'atil') ?> 1x, <?php echo os_path('drawing-2@2x.png', 'atil') ?> 2x" width="1600" height="1008">
+				</li>
+				<li class="img-4">
+					<img src="<?php echo os_path('drawing-3.png', 'atil') ?>" width="1212" height="978" srcset="<?php echo os_path('drawing-3.png', 'atil') ?> 1x, <?php echo os_path('drawing-3@2x.png', 'atil') ?> 2x" width="1600" height="1008">
+				</li>
+				<li class="img-3">
+					<img src="<?php echo os_path('drawing-4.png', 'atil') ?>" width="1212" height="978" srcset="<?php echo os_path('drawing-4.png', 'atil') ?> 1x, <?php echo os_path('drawing-4@2x.png', 'atil') ?> 2x" width="1600" height="1008">
+				</li>
+			</ul>
+		</div>
+
+		<div class="outer-container">
+			<figcaption class="inner-container">Initial sketches of policy effects layered on the urban landscape</figcaption>
+		</div>
+	</figure>
+
+	<div class="outer-container bleed push-triple">
+		<div class="inner-container">
+			<p>We designed a practical binder in a striking color to capture the attention of a busy public official. The content was divided by implementation considerations, rather than subject matter, to anticipate the steps local leaders would need to take.</p>
+		</div>
+	</div>
+
+	<figure class="aligncenter cycle-images">
+		<div class="outer-container">
+			<img class="img-1" src="<?php echo os_path('chapters.png', 'atil') ?>" width="1212" height="978" srcset="<?php echo os_path('chapters.png', 'atil') ?> 1x, <?php echo os_path('chapters@2x.png', 'atil') ?> 2x" width="1600" height="1008" alt="TransitCenter’s All Transportation is Local chapters" >
+		</div>
+
+		<div class="outer-container">
+			<figcaption class="inner-container">TransitCenter’s All Transportation is Local chapters</figcaption>
+		</div>
+	</figure>
+
+	<div class="outer-container">
+		<div class="inner-container">
+			<p>The individual hole-punched sections allow for easy digestion while also leaving room for the publication to be updated with new or revised sections when TransitCenter has new findings in the future.</p>
+			<p>We optimized the publication for online consumption with interactive triggers to even further underscore the cumulative effect of policies.</p>
+		</div>
+
+			<div class="browser push-triple" >
+				<div class="browser-bar"></div>
+				<div class="browser-window black">
+					<div class="browser-inner">
+						<img src="<?php echo os_path('streets-base.png', 'atil'); ?>" srcset="<?php echo os_path('streets-base.png', 'atil'); ?> 1x, <?php echo os_path('streets-base@2x.png', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+				</div>
+			</div>
+			<figcaption class="inner-container">TransitCenter’s All Transportation is Local chapters</figcaption>
+
+
+			<div class="inner-container">
+				<p>TransitCenter distributed <em>All Transportation is Local</em> to policymakers and transportation leaders around the country. TransitCenter’s useful, considered content married with our arresting and functional design elicited an enthusiastic response from policymakers on social media.</p>
+			</div>
+	</div>
+
+	<div class="outer-container bleed push-triple">
+			<div class="flex-blocks">
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person66.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person66.jpg', 'atil'); ?> 1x, <?php echo os_path('Person66@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person65.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person65.jpg', 'atil'); ?> 1x, <?php echo os_path('Person65@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person64.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person64.jpg', 'atil'); ?> 1x, <?php echo os_path('Person64@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person62.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person62.jpg', 'atil'); ?> 1x, <?php echo os_path('Person62@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person67.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person67.jpg', 'atil'); ?> 1x, <?php echo os_path('Person67@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person68.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person68.jpg', 'atil'); ?> 1x, <?php echo os_path('Person68@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person69.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person69.jpg', 'atil'); ?> 1x, <?php echo os_path('Person69@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person70.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person70.jpg', 'atil'); ?> 1x, <?php echo os_path('Person70@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person71.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person71.jpg', 'atil'); ?> 1x, <?php echo os_path('Person71@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person72.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person72.jpg', 'atil'); ?> 1x, <?php echo os_path('Person72@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person73.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person73.jpg', 'atil'); ?> 1x, <?php echo os_path('Person73@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+
+				<div class="column column-3">
+					<div class="block-image">
+						<img src="<?php echo os_path('Person74.jpg', 'atil'); ?>" srcset="<?php echo os_path('Person74.jpg', 'atil'); ?> 1x, <?php echo os_path('Person74@2x.jpg', 'atil'); ?> 2x" width="850" height="780">
+					</div>
+					<div class="block-caption">
+						<p>Ydanis Rodriguez</p>
+						<p><em>NYC Council Transport</em></p>
+						<p>Committee Chair</p>
+					</div>
+				</div>
+			</div>
+	</div>
+
+
+
+
+
+	<div class="outer-container">
+		<div class="inner-container">=
+			<p class="h2 future-A"><a href="http://transitcenter.org/" class="cta-link" target="_blank">transitcenter.org <span class="icon arnhem rotate-cc-45">&rarr;</span></a></p>
+		</div>
+	</div>
+
+</section>
