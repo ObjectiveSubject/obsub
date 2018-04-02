3595555443282b084f6ce2eac8e5d415dc0cf347
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-04-02 12:02:16 +0200

    build new subscribe form

--- footer.php
+++ footer.php
@@ -12,7 +12,26 @@
 
 	<footer id="colophon" class="site-footer bg-default" role="contentinfo">
 		<div class="outer-container">
+
 			<div class="inner-container">
+				<p class="h1">Get our periodic email updates</p>
+				<form id="subForm" class="js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="5B5E7037DA78A748374AD499497E309EB2458E90C43B586B223EEA4E239EEE513766D33A967F0A3CD89493E329D3A9FB70B520762DED60A2E37E3A8A03AB73DC">
+					<div class="form-field align-md">
+						<label for="fieldName" style="min-width:150px"><em>Your name</em></label>
+						<input id="fieldName" name="cm-name" type="text" />
+					</div>
+					<div class="form-field align-md mt-nudge">
+						<label for="fieldEmail" style="min-width:150px"><em>Email Address</em></label>
+						<input id="fieldEmail" class="js-cm-email-input" name="cm-djdjtk-djdjtk" type="email" required /> 
+					</div>
+					<div class="mt-nudge text-right">
+						<button class="js-cm-submit-button" type="submit">Subscribe &nbsp;&nbsp;&#8250;</button> 
+					</div>
+				</form>
+				<script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
+			</div>
+
+			<div class="inner-container mt-5">
 				<ul class="site-footer__list list-unstyled">
 					<li><a href="<?php echo site_url('work') ?>" >Work</a></li>
 					<!-- <li><a href="<?php echo site_url('views') ?>">Views</a></li> -->
