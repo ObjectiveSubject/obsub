2a8cbb018e024c6dda12e09d7825a9b1ca81037c
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-04-02 17:31:40 +0200

    unify look of case study preview

--- module-case-study-preview.php
+++ module-case-study-preview.php
@@ -23,9 +23,14 @@ foreach ($case_studies as $study) :
             </div>
             <div class="section-content">
                 <div class="outer-container">
-                    <h1 class="case-study-title inner-container mt-nudge super"><a href="<?php echo get_permalink( $study ); ?>"><?php echo get_the_title($study) ?></a></h1>
-                    <h3 class="case-study-subtitle inner-container"><?php echo get_the_excerpt( $study ); ?></h3>
-                    <p class="inner-container mt-nudge"><a href="<?php echo get_permalink( $study ); ?>" title="View case study"><em>View case study &rarr;</em></a></p>
+                    <div class="inner-container">
+
+                        <h2 class="h1 mt-nudge super"><?php echo get_the_excerpt( $study ); ?></h2>
+                        <h3 class="page-subtitle mt-3"><?php echo get_field('case_study_subtitle', $study->ID); ?></h3>
+                        
+                        <p><a href="<?php echo get_permalink( $study ); ?>" class="button-ghost" style="margin-top:0.5em">view case study &nbsp; &#8250;</a></p>
+
+                    </div>
                 </div>
             </div>
         </div>
