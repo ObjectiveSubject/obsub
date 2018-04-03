49db5f4d64907031756740d726d8110f3a850fa8
a-tawfik <taamer@gmail.com>
2018-04-03 17:54:28 -0400

    qa refinments

--- page-contact.php
+++ page-contact.php
@@ -10,20 +10,19 @@ get_header(); ?>
 <?php the_post(); ?>
 
 	<section id="about-intro" class="about-intro page-section">
-	
+
 		<div class="intro-content section-content">
 			<div class="outer-container">
 
 				<div class="os-row">
 					<div class="os-column os-span-10-md os-span-6-lg os-offset-1-md os-offset-2-lg">
-						<h1 class="h1 page-title"><?php the_title() ?></h1>
 						<?php the_content() ?>
 					</div>
 				</div>
 
 			</div>
 		</div>
-	
+
 	</section>
 
 	<div class="outer-container">
@@ -55,36 +54,36 @@ get_header(); ?>
 
 		<div class="grid mt-5">
 
-			<img src="<?php echo os_path('contact-photo-1-bw.jpg', 'contact'); ?>" 
-				 width="2048" height="1200" 
+			<img src="<?php echo os_path('contact-photo-1-bw.jpg', 'contact'); ?>"
+				 width="2048" height="1200"
 				 class="grid-item" />
-			
-			<img src="<?php echo os_path('contact-photo-2-bw.jpg', 'contact'); ?>" 
-				 width="822"  height="1200" 
+
+			<img src="<?php echo os_path('contact-photo-2-bw.jpg', 'contact'); ?>"
+				 width="822"  height="1200"
 				 class="grid-item span-5-md" />
-			
-			<img src="<?php echo os_path('contact-photo-3-bw.jpg', 'contact'); ?>" 
-				 width="1172" height="1200" 
+
+			<img src="<?php echo os_path('contact-photo-3-bw.jpg', 'contact'); ?>"
+				 width="1172" height="1200"
 				 class="grid-item span-7-md" />
-			
-			<img src="<?php echo os_path('contact-photo-4-bw.jpg', 'contact'); ?>" 
-				 width="2048" height="1200" 
+
+			<img src="<?php echo os_path('contact-photo-4-bw.jpg', 'contact'); ?>"
+				 width="2048" height="1200"
 				 class="grid-item" />
-			
-			<img src="<?php echo os_path('contact-photo-5-bw.jpg', 'contact'); ?>" 
-				 width="2048" height="1200" 
+
+			<img src="<?php echo os_path('contact-photo-5-bw.jpg', 'contact'); ?>"
+				 width="2048" height="1200"
 				 class="grid-item" />
-			
-			<img src="<?php echo os_path('contact-photo-6-bw.jpg', 'contact'); ?>" 
-				 width="1002" height="1200" 
+
+			<img src="<?php echo os_path('contact-photo-6-bw.jpg', 'contact'); ?>"
+				 width="1002" height="1200"
 				 class="grid-item span-6-md" />
-			
-			<img src="<?php echo os_path('contact-photo-7-bw.jpg', 'contact'); ?>" 
-				 width="883"  height="1057" 
+
+			<img src="<?php echo os_path('contact-photo-7-bw.jpg', 'contact'); ?>"
+				 width="883"  height="1057"
 				 class="grid-item span-6-md" />
-			
-			<img src="<?php echo os_path('contact-photo-8-bw.jpg', 'contact'); ?>" 
-				 width="2048" height="1200" 
+
+			<img src="<?php echo os_path('contact-photo-8-bw.jpg', 'contact'); ?>"
+				 width="2048" height="1200"
 				 class="grid-item" />
 
 		</div>
