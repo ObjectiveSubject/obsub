ccb7f6297eed5c1f7983eaaf906a5bd2a944eb2a
a-tawfik <taamer@gmail.com>
2018-04-03 11:53:07 -0400

    404 spacing

--- 404.php
+++ 404.php
@@ -15,8 +15,8 @@ get_header(); ?>
 			<div class="section-content">
 				<div class="outer-container">
 					<div class="inner-container">
-						<h1>looks like we can't find what you're looking for...</h1>
-						<img src="<?php echo os_path('pleaseclap.gif', '404') ?>" width="1600" height="1008" alt="Please Clap...">
+						<h1>Looks like we can't find what you're looking for, but...</h1>
+						<p><img src="<?php echo os_path('pleaseclap.gif', '404') ?>" width="1600" height="1008" alt="Please Clap..."></p>
 
 						<p><a class="button" href="/">home   ›</a></p>
 					</div>
