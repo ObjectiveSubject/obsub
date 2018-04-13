3f0ccaf1b6ee75ce1df3b116dcdf258d6e6e8b18
David Jalbert-Gagnier <david@objectivesubject.com>
2018-04-13 15:43:49 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@ get_header(); ?>
 			<div class="section-content">
 				<div class="outer-container">
 					<div class="inner-container">
-						<h1>Looks like we can't find what you're looking for, but...</h1>
+						<h1>Looks like we can’t find what you’re looking for, but...</h1>
 						<p><img src="<?php echo os_path('pleaseclap.gif', '404') ?>" width="1600" height="1008" alt="Please Clap..."></p>
 
 						<p><a class="button" href="/">home   ›</a></p>
